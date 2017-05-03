606ef8e62f337327dd5631a0c9f0ee7e5822d338
Raju Sharan <raaz2053@gmail.com>
2017-05-03 22:15:57 +0545

    Add files via upload

---
+++ irRemoteORG.ino
@@ -0,0 +1,76 @@
+#include <IRremote.h>
+ 
+int RECV_PIN = 11; // the pin where you connect the output pin of TSOP4838
+int led4 = 10;
+int led3 = 9;
+int led2 = 8;
+int led1 = 7;
+int itsONled[] = {0,0,0,0,0};
+/* the initial state of LEDs is OFF (zero) 
+the first zero must remain zero but you can 
+change the others to 1's if you want a certain
+led to light when the board is powered */
+#define code1  22695 //F
+#define code2  41055 //B
+#define code3  32895 //L
+#define code4  49215 //R
+IRrecv irrecv(RECV_PIN);
+ 
+decode_results results;
+ 
+void setup()
+{
+  Serial.begin(9600);   // you can comment this line
+  irrecv.enableIRIn();  // Start the receiver
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  
+}
+ 
+void loop() {
+  if (irrecv.decode(&results)) {
+    unsigned int value = results.value;
+    switch(value) {
+       case code1:
+         if(itsONled[1] == 1) {        // if first led is on then
+            digitalWrite(led1, LOW);   // turn it off when button is pressed
+            itsONled[1] = 0;           // and set its state as off
+         } else {                      // else if first led is off
+             digitalWrite(led1, HIGH); // turn it on when the button is pressed
+             itsONled[1] = 1;          // and set its state as on
+         }
+          break; 
+       case code2:
+         if(itsONled[2] == 1) {
+            digitalWrite(led2, LOW);
+            itsONled[2] = 0;
+         } else {
+             digitalWrite(led2, HIGH);
+             itsONled[2] = 1;
+         }
+          break;
+       case code3:
+         if(itsONled[3] == 1) {
+            digitalWrite(led3, LOW);
+            itsONled[3] = 0;
+         } else {
+             digitalWrite(led3, HIGH);
+             itsONled[3] = 1;
+         }
+          break;  
+       case code4:
+         if(itsONled[4] == 1) {
+            digitalWrite(led4, LOW);
+            itsONled[4] = 0;
+         } else {
+             digitalWrite(led4, HIGH);
+             itsONled[4] = 1;
+         }
+          break;        
+    }
+    Serial.println(value); // you can comment this line
+    irrecv.resume(); // Receive the next value
+  }
+}
